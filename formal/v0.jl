f67400310a0859fa77a74070651a9bc0c3a78865
riczzhou <ericzhouzw@gmail.com>
2022-06-22 23:56:08 +0100

    fix typo and update notes

--- formal/v0.jl
+++ formal/v0.jl
@@ -83,7 +83,7 @@ function InverseBlockBiagonalUpper(U, bw)
         Yt[:, i-bw+1:i] = Yti
     end
     D0 = U[1:r, 1:r]
-    D0inv = qr(D0) \ I
+    # D0inv = qr(D0) \ I
     X0 = X[1:r, :]
     Yt0 = invR(D0 * X0)
     Yt[:, 1:r] = Yt0
@@ -159,10 +159,12 @@ end
 
     @testset "Inverse of BlockBidiagonal U" begin
         for _ in 1:5
-            n = rand(1:1000)
+            n = rand(2:1000)
             bw = rand(1:n-1)
             U = big.(BandedMatrix(rand(n, n), (0, bw))) + I
             Uinv = InverseBlockBiagonalUpper(U, bw)
+            # @time InverseBlockBiagonalUpper(U, bw)
+            # @time qr(U) \ I
             @test U * Uinv ≈ I
             @test Uinv * U ≈ I
         end
